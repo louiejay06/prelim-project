e1acd049fba40a2d0c1aa427ffd44d2c5429bda1
louiejay06 <Louiejayaurestila@gmail.com>
2022-09-27 20:52:04 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <style src="{{asset('js/bootstrap.min.js')}}"></style>
+</head>
+
+<body>
+<h1>This is my App</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero perferendis optio illum, adipisci odit alias, velit, odio aliquid doloribus necessitatibus fugiat? Totam et sequi fugit? Quos eligendi aliquid temporibus reprehenderit.</p>
+        <a href="{{url('/')}}">Home</a>
+        <a href="{{url('app')}}">Application</a>
+        <a href="{{url('creator')}}">Developer</a>
+
+       
+   
+</body>
+</html>
---
+++ resources/views/creator.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <style src="{{asset('js/bootstrap.min.js')}}"></style>
+</head>
+<style>
+    body{
+        margin-left: 350px;
+ 
+    }
+    a{
+        font-size: 50px;
+        margin: 50px;   
+
+    }
+    h1{
+        font-size: 50px;
+        margin: 50px;   
+    }
+
+    p{  font-size: 50px;
+        margin: 50px;   
+
+    }
+</style>
+<body>
+<h1>Developer</h1>
+        <p>
+            Name: Louie Jay Auristila
+        </p>
+        <a href="{{url('/')}}">Home</a>
+        <a href="{{url('app')}}">Application</a>
+        <a href="{{url('creator')}}">Developer</a>
+
+       
+   
+</body>
+</html>
